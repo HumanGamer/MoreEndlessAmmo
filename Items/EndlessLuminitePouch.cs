1e47b708eae63293de81b2f74dcee05ae4c66e53
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2018-05-11 15:48:39 -0400

    Added Endless Luminite Pouch

---
+++ Items/EndlessLuminitePouch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MoreEndlessAmmo.Items
+{
+	public class EndlessLuminitePouch : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Endless Luminite Pouch");
+		}
+
+		public override void SetDefaults()
+		{
+			item.shootSpeed = 2f;
+			item.shoot = 638;
+			item.damage = 20;
+			item.width = 26;
+			item.height = 34;
+			item.ammo = AmmoID.Bullet;
+			item.knockBack = 3f;
+			item.value = Item.sellPrice(0, 2, 0, 0);
+			item.ranged = true;
+			item.rare = 9;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.MoonlordBullet, 3996);
+			recipe.AddTile(TileID.CrystalBall);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
