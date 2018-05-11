34c76cce37294cb0b6dbeefb6b5e46ecba6abe95
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2018-05-11 16:07:03 -0400

    Added Endless Party Pouch

---
+++ Items/EndlessPartyPouch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MoreEndlessAmmo.Items
+{
+	public class EndlessPartyPouch : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Endless Party Pouch");
+		}
+
+		public override void SetDefaults()
+		{
+			item.shootSpeed = 5.1f;
+			item.shoot = 284;
+			item.damage = 10;
+			item.width = 26;
+			item.height = 34;
+			item.ammo = AmmoID.Bullet;
+			item.knockBack = 5f;
+			item.value = Item.sellPrice(0, 2, 0, 0);
+			item.ranged = true;
+			item.rare = 3;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.PartyBullet, 3996);
+			recipe.AddTile(TileID.CrystalBall);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
