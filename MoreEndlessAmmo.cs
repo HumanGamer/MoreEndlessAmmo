ca3b351d5d0b6ed5bf6874d6dd0d87d5bb5b526f
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2018-05-11 13:36:41 -0400

    Added Main Mod File and Project

---
+++ MoreEndlessAmmo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace MoreEndlessAmmo
+{
+    public class MoreEndlessAmmo : Mod
+    {
+	    internal static MoreEndlessAmmo Instance;
+
+		public MoreEndlessAmmo()
+		{
+			Properties = new ModProperties()
+			{
+				Autoload = true,
+				AutoloadBackgrounds = true,
+				AutoloadGores = true,
+				AutoloadSounds = true
+			};
+		}
+
+	    public override void Load()
+	    {
+		    Instance = this;
+	    }
+    }
+}
