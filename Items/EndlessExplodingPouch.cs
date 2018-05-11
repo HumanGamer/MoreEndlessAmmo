6371ceef044efdac1fcf314afaa2bc61d9684898
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2018-05-11 16:20:35 -0400

    Added Endless Exploding Pouch

---
+++ Items/EndlessExplodingPouch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MoreEndlessAmmo.Items
+{
+	public class EndlessExplodingPouch : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Endless Exploding Pouch");
+		}
+
+		public override void SetDefaults()
+		{
+			item.shootSpeed = 4.7f;
+			item.shoot = 286;
+			item.damage = 10;
+			item.width = 26;
+			item.height = 34;
+			item.ammo = AmmoID.Bullet;
+			item.knockBack = 6.6f;
+			item.value = Item.sellPrice(0, 2, 0, 0);
+			item.ranged = true;
+			item.rare = 3;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.ExplodingBullet, 3996);
+			recipe.AddTile(TileID.CrystalBall);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
