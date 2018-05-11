466308e03e45b96fa806318549162ab5c58712e4
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2018-05-11 14:18:35 -0400

    Added Endless Crystal Pouch

---
+++ Items/EndlessCrystalPouch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MoreEndlessAmmo.Items
+{
+	public class EndlessCrystalPouch : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Endless Crystal Pouch");
+		}
+
+		public override void SetDefaults()
+		{
+			item.shootSpeed = 5f;
+			item.shoot = 89;
+			item.damage = 9;
+			item.width = 8;
+			item.height = 8;
+			item.ammo = AmmoID.Bullet;
+			item.knockBack = 1f;
+			item.value = 30;
+			item.ranged = true;
+			item.rare = 3;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.CrystalBullet, 3996);
+			recipe.AddTile(TileID.CrystalBall);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
