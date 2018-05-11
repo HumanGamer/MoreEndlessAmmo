69c5c6aca295cab21b8bbbf6b65c25ea24ae16f2
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2018-05-11 15:35:37 -0400

    Added Endless Nano Pouch

---
+++ Items/EndlessNanoPouch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MoreEndlessAmmo.Items
+{
+	public class EndlessNanoPouch : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Endless Nano Pouch");
+		}
+
+		public override void SetDefaults()
+		{
+			item.shootSpeed = 4.6f;
+			item.shoot = 285;
+			item.damage = 10;
+			item.width = 26;
+			item.height = 34;
+			item.ammo = AmmoID.Bullet;
+			item.knockBack = 3.6f;
+			item.value = Item.sellPrice(0, 2, 0, 0);
+			item.ranged = true;
+			item.rare = 3;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.NanoBullet, 3996);
+			recipe.AddTile(TileID.CrystalBall);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
